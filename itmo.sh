56d8766d0dc22c8452df298b08f7a43644f6897c
Anara <anara.1905@gmail.com>
2017-06-07 20:01:13 +0300

    changes

--- itmo.sh
+++ itmo.sh
@@ -1,29 +1,91 @@
 #!/bin/sh
 
-
-echo "Ethereum Wallet Transfer"
-echo "From"  $1
-echo "To" $2
-echo "Ammount" $3 
-
-
-echo "Your Wallet is:"
-./geth --testnet --fast -exec "eth.accounts" attach 
-
-
-
-echo "Transaction"
-
-
 pfile="./passwd"
 PASSWD=$(cat "$pfile")
 
+echo "Ethereum Wallet Transfer"
 
-./geth --exec "personal.unlockAccount('$1', '$PASSWD')" attach > /dev/null
-./geth --testnet --fast -exec "eth.sendTransaction({from:'$1', to: '$2', value: '$3'})" attach
+ETH=$(which geth)  
+check_geth() {
+    STATUS=""
+    if [ -z $ETH ] && [ ! "$(ps -A | grep ether*)" ];  
+    then
+        STATUS="You need to install Ethereum CLI based on GoLang, or run Ethereum Wallet App"
+    else 
+        STATUS="OK"
+    fi
+    echo $STATUS
+}
+
+echo "Transaction"
+
+transaction(){
+	SENDER=$sen
+	RECEIVER=$rec
+	AMOUNT=$amount
+	./geth --exec "personal.unlockAccount('$sen', '$PASSWD')" attach > /dev/null	
+	TRANSACTION=`./geth --testnet --fast -exec "eth.sendTransaction({from: '$SENDER', to: '$RECEIVER', value: web3.toWei('$AMOUNT', 'ether')})" attach`
+	echo $TRANSACTION
+}
 
 
-#./geth --testnet --fast -exec "eth.sendTransaction({from:'$1', to: '$2', value: '$3'})" attach
+help () {
+    printf "Script: "$0" provides the possibility to send ether from one account to another\n"
+}
+
+
+
+cli_main(){
+
+if [ ! -z $sen ] && [ $sen = "-h" ];
+    then
+        help
+        exit
+fi
+
+STATUS=$(check_geth)
+    if [[ $STATUS != *"OK" ]] || $([ ! -z $sen ] );
+    then
+        echo $STATUS
+        exit 
+fi
+
+
+
+while true
+do
+	echo -n "Enter SENDER account:"
+	read SENDER
+	#echo "Your Wallet is: $sen"
+
+	#./geth --testnet --fast -exec "eth.accounts" attach
+
+	echo -n "Enter RECEIVER account:"
+	read RECEIVER
+	#echo "Your Wallet is: $rec"
+
+	echo -n "Enter AMOUNT to send:"
+	read AMOUNT
+	#echo "Amount to send is : $amount"
+
+	TRANSACTION="$(transaction $SENDER $RECEIVER $AMOUNT)"
+	echo "Transaction code is:"
+	echo $TRANSACTION
+
+	printf "Do you want make another transaction? (Y/N).\n"
+	read ANS
+	if [ $ANS != "Y" ];
+        then
+            exit
+	fi
+
+done
+
+} 
+
+
+cli_main $*
+
 
  
 
